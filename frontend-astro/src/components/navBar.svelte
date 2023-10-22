7b50c3b86fb6b0cd583c17db55565c3284f5a0fc
Rashaad <rashaadzma@gmail.com>
2023-10-22 21:10:48 +0530

    did some things

--- frontend-astro/src/components/navBar.svelte
+++ frontend-astro/src/components/navBar.svelte
@@ -43,7 +43,9 @@
                     : "text-gray-300 hover:bg-gray-700 hover:text-white",
                   "rounded-md px-3 py-2 text-sm font-medium"
                 )}
-                aria-current={item.href === win?.location.hash ? "page" : undefined}
+                aria-current={item.href === win?.location.hash
+                  ? "page"
+                  : undefined}
               >
                 {item.name}
               </a>
