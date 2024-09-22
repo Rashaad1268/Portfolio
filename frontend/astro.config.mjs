7dbf9f69eec3039997a9477f6dbc897fe24faaef
Rashaad <rashaadzma@gmail.com>
2024-09-22 08:07:58 +0530

    add vercel adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), svelte()]
+  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  output: "server",
+  adapter: vercel()
 });
