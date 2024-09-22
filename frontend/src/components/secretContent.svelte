8539583e55da009121224b10eec8e3b1bf31e0ec
Rashaad <rashaadzma@gmail.com>
2024-09-22 21:19:07 +0530

    did a thing...

---
+++ frontend/src/components/secretContent.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { onMount } from "svelte";
+
+    let showContent = false;
+    export let inverse = false;
+
+    onMount(() => {
+        showContent = sessionStorage.showSecretContent === "true";
+    });
+</script>
+
+{#if showContent}
+    {#if !inverse}
+        <slot />
+    {/if}
+{:else if inverse}
+    <slot />
+{/if}
